b8c610fc2d225fa7c43ff690f6f6a78136f74ef5
IvanSHarianja <100180168+IvanSHarianja@users.noreply.github.com>
2023-03-18 11:41:27 +0700

    Add files via upload

---
+++ Pertemuan4/BujurSangkar.java
@@ -0,0 +1,20 @@
+package org.bangundatar ;
+
+import org.poligon.poligon ;
+
+public class BujurSangkar extends poligon{
+	private double panjangSisi ;
+	
+	public BujurSangkar(double panjangSisi){
+		this.jumlahSisi = 4 ;
+		this.panjangSisi = panjangSisi ;
+	}
+	
+	public double hitungLuas(){
+		return panjangSisi * panjangSisi ;
+	}
+	
+	public double getPanjangSisi(){
+		return this.panjangSisi ;
+	}
+}
