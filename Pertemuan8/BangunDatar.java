a3dd122d6c029e306d013e0ebd2ea20599da1eb0
IvanSHarianja <100180168+IvanSHarianja@users.noreply.github.com>
2023-05-31 13:11:01 +0700

    Add files via upload

---
+++ Pertemuan8/BangunDatar.java
@@ -0,0 +1,10 @@
+/**
+ * File      	: BangunDatar.java 
+ * Penulis 		: Ivan S Harianja
+ * Deskripsi 	: Kelas abstrak bangun datar
+ *                
+ */
+ 
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
